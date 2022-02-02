1bc84ff3d417164541db3355dae142833199a73f
Alex Hosier <alexhos75@gmail.com>
2022-02-02 19:55:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
-require('dotenv').config()
+// require('dotenv').config() // DEV PURPOSE
 const app = express()
 
 // Create the connection
